3f6343ae8e5b3b66b64154df147a28033d17d5df
vleft02 <vleftakis@gmail.com>
2023-11-25 14:57:17 +0200

    form validaton

--- constraint-validation.js
+++ constraint-validation.js
@@ -29,98 +29,147 @@ window.addEventListener('load', function validateForm()
             checkMaxInput(120, this.value.length,event)
         });
 
-        // let fullname = document.getElementById('name');
-        // fullname.oninvalid = function(event)
-        // {
-        //     event.target.setCustomValidity();
-        // }
 
-        document.getElementById('signUpForm').addEventListener('submit', function(event)
-        {
-            event.preventDefault();
+
         
+        // We get the two phone inputs and assign an input event to the second one 
+        // so when it is given we can check if the are the same numbner and show the appropriate message
+        let phone = document.getElementById('tel');
+        let mobilePhone = document.getElementById('mobile-tel'); 
+        mobilePhone.addEventListener('input', function(event) {
             
-            let phone = document.getElementById('tel');
-            let mobilePhone = document.getElementById('mobile-tel'); 
-
-            if (phone.checkValidity() && mobilePhone.checkValidity())
+            if (phone.value === event.target.value)
+            {
+                phone.setCustomValidity('Παραχωρήσατε διπλότυπο αριθμό επικοινωνίας');
+                mobilePhone.setCustomValidity('Παραχωρήσατε διπλότυπο αριθμό επικοινωνίας');
+                phone.classList.add('invalid');
+                mobilePhone.classList.add('invalid');  
+            }
+            else
             {
                 phone.setCustomValidity('');
                 mobilePhone.setCustomValidity('');
-                if (phone.value == mobilePhone.value)
-                {
-                    phone.setCustomValidity('Παραχωρήσατε διπλότυπο αριθμό επικοινωνίας');
-                    phone.setCustomValidity('Παραχωρήσατε διπλότυπο αριθμό επικοινωνίας');
+                if (phone.classList.contains('invalid')) {
+                    phone.classList.remove('invalid');
                 }
-            }
-
-            let password = document.getElementById('password');
-            let passwordConfirmation = document.getElementById('password-confirmation');
-
-            if (password.checkValidity() && passwordConfirmation.checkValidity())
-            {
-                password.setCustomValidity('');
-                passwordConfirmation.setCustomValidity('');
-                if (password.value != passwordConfirmation.value)
-                {
-                    password.setCustomValidity('Οι κωδικοί δεν ταιριάζουν');
-                    passwordConfirmation.setCustomValidity('Οι κωδικοί δεν ταιριάζουν');
+                if (mobilePhone.classList.contains('invalid')) {
+                    mobilePhone.classList.remove('invalid');
                 }
+      
             }
+        });
 
-
-
-            // let fieldValues = new Map();
-
-            // for (let input of inputs)
-            // {
-            //     fieldValues.set(input.name, input.value);
-            //     console.log(input.name + ": "+ input.value)
-            // }
-            
-            // for (let textarea of textareas)
-            // {
-            //     fieldValues.set(textarea.name, textarea.value);
-            // }
-
-            // for (let select of selects )
-            // {
-            //     fieldValues.set(select.name, select.value);
-            // }
-            // fieldValues.delete('search');
-            
-
-            // if (fieldValues.get('name').checkValidity()){
-            //     fieldValues.get('name').
-            // }
-            // else
-            // {
-
-            // }
-
-            // let fullname = document.getElementById('name');
-            // if (!fullname.checkValidity())
-            // {
-            //     fullname.setCustomValidity('Πληκτρλογήστε ένα έγκυρο όνοματεπώνυμο με όχι λιγοτερους απο 4 λατινικούς ή ελληνικούς χαρακτήρες.');
-            // }
+        let password = document.getElementById('password');
+        let passwordConfirmation = document.getElementById('password-confirmation');
+        
+        // We get the two password inputs and assign an input event to the second one 
+        // so when it is given a value we can check if the passwords match and show the appropriate message
+        passwordConfirmation.addEventListener('input',function(event)
+        {
+                console.log(event.target.value);
+                if (checkMatchingPassword(password.value,event.target.value))
+                {
+                    passwordConfirmation.setCustomValidity('');
+                    if (passwordConfirmation.classList.contains('invalid')) {
+                        passwordConfirmation.classList.remove('invalid');
+                    }
+                    if (password.classList.contains('invalid')) {
+                        password.classList.remove('invalid');
+                    }
+                }
+                else
+                {
+                    passwordConfirmation.setCustomValidity('Οι κωδικοι δεν ταιριάζουν');
+                    password.classList.add('invalid');
+                    passwordConfirmation.classList.add('invalid');
+                }
         })
-    })
+
+
+        let birthday = document.getElementById('birthday')
+        
+        //we get the dat input and add an event listener so when its valeu is changed we check wether th date
+        // is acceptable or not
+        birthday.addEventListener('input',function(event)
+        {
+            console.log(event.target.value);
+                
+            let minDate = new Date();
+            minDate.setFullYear(minDate.getFullYear() - 18);
+            let birthdate = new Date(event.target.value)
+            if (birthdate.getFullYear()> minDate.getFullYear() || birthdate.getFullYear()< 1900 )
+            {
+                birthday.setCustomValidity('Η εγγραφή επιτρέπεται μόνο για χρήστες άνω των 18');
+                birthday.classList.add('invalid');
+
+            }
+            else
+            {
+                birthday.setCustomValidity('');
+                if (birthday.classList.contains('invalid')) {
+                    birthday.classList.remove('invalid');
+                }
+
+            }
+
+        })
+
+        // prevents POST in order to debug the form
+        let form = document.getElementById('signUpForm');
+        form.addEventListener('submit', function(event)
+        {
+            event.preventDefault();
+            
+        })              
+
+})
 
 
 
 
-
-function checkMaxInput(maxInputAllowed, input,event)
+// We check wether a maximum input is allowed and prevet typing if so
+function checkMaxInput(maxInputAllowed, input,keyStorkeEvent)
 {
 
     if (input>=maxInputAllowed) {
-        event.preventDefault();
+        keyStorkeEvent.preventDefault();
         alert('Το Μέγιστο πλήθος των χαρατήρων που επιτρέπετε είναι '+maxInputAllowed);
     }
 }
 
-function stringValidityCheck(inputValue, regex)
-{
-    return inputValue.match(regex);
 
+// We check wether the 2 password given are mathcing and return true if that is the case
+function checkMatchingPassword(password1, password2)
+{
+    if (password1 === password2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
+
+
+
+
+// function validateAtLeastOneCheckBox(checkboxes)
+// {
+//     let counter=0;
+//     for (let checkbox of checkboxes)
+//     {
+//         if(checkbox.checked)
+//         {
+//             counter++;
+//         }
+//     }
+//     if (counter===0)
+//     {
+//         checkboxes[0].setCustomValidity('Επιλέξτε τουλάχιστον ένα πεδιο');
+//     } 
+//     else{
+//         checkboxes[0].setCustomValidity('');
+//     }
+
+// }
